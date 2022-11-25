71bea475286d9d3f8f34ff727a76ccae62a27f32
alexsandrocl13 <alexsandrocl13@gmail.com>
2022-11-25 20:39:48 -0300

    variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estude a classe String que representa texto na aplicação
+        
+        String meuNome = "Alex Lima";
+        
+        final double salarioMinimo = 2.500;
+
+        short numeroCurto = 1;  
+        int numeroNormal1 = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal1;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+        System.out.println(meuNome);
+
     }
 }
